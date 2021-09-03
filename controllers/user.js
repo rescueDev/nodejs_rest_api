85c219ac909ac08e2609c2a3088ee283982f78d3
sa-borgia3 <sa.borgia3@gmail.com>
2021-09-03 19:49:14 +0200

    refactoring

--- controllers/user.js
+++ controllers/user.js
@@ -1,95 +1,103 @@
+// Import
 const faker = require("faker");
 const User = require("../models/user");
 const Car = require("../models/car");
 const mongoose = require("mongoose");
 
-// Read All
-exports.getUsers = (req, res, next) => {
-  User.find()
-    .then((users) => {
-      //Set header to retrive json
-      res.setHeader("Content-Type", "application/json");
-      res.send(JSON.stringify({ users }));
-    })
-    .catch((err) => console.log(err));
+//------- READ ALL USERS--------//
+exports.getUsers = async (req, res, next) => {
+  //Set header to retrive json
+  res.setHeader("Content-Type", "application/json");
+
+  try {
+    //find all Users
+    const users = await User.find();
+
+    //response with users json
+    res.send(JSON.stringify({ users }));
+  } catch (err) {
+    res.send(JSON.stringify({ error: error }));
+  }
 };
-exports.generateUsers = (req, res, next) => {
-  //check if any user already generated
-  User.find()
-    .then((users) => {
-      console.log(users);
-      return users;
-    })
-    .then((users) => {
-      //check if users array is not empty
-      if (users.length > 0) {
-        console.log("users already generated", users);
-        return;
-      } else {
-        console.log("generating users");
 
-        //Faker generate user
-        for (let index = 0; index < 10; index++) {
-          let car = new Car({
-            _id: new mongoose.Types.ObjectId(),
-            brand: faker.vehicle.manufacturer(),
-            model: faker.vehicle.model(),
-            year: Math.floor(Math.random() * 2021) + 1992,
-            specs: {
-              power: Math.floor(Math.random() * 180) + 70,
-              fuel: faker.vehicle.fuel(),
-            },
-          });
+//---- FAKE GENERATE RANDOM USERS AND CARS --------//
+exports.generateUsers = async (req, res, next) => {
+  try {
+    //find all users if present
+    const users = await User.find();
 
-          car.save((err, data) => {
-            if (err) {
-              console.log(err);
-            }
-          });
+    //check if any user already generated
+    if (users.length > 0) {
+      console.log("users already generated", users);
+      return;
+    } else {
+      console.log("generating users");
 
-          let user_item = new User({
-            name: faker.name.firstName(),
-            username: faker.internet.userName(),
-            email: faker.internet.email(),
-            gender: faker.name.gender(),
-            age: Math.floor(Math.random() * 90) + 1,
-            address: {
-              street: faker.address.streetName(),
-              city: faker.address.city(),
-              country: faker.address.country(),
-            },
-            car: car,
-          });
+      //Faker generate user
+      for (let index = 0; index < 10; index++) {
+        let car = new Car({
+          _id: new mongoose.Types.ObjectId(),
+          brand: faker.vehicle.manufacturer(),
+          model: faker.vehicle.model(),
+          year: Math.floor(Math.random() * 2021) + 1992,
+          specs: {
+            power: Math.floor(Math.random() * 180) + 70,
+            fuel: faker.vehicle.fuel(),
+          },
+        });
 
-          //save users
-          user_item.save((err, data) => {
-            if (err) {
-              console.log(err);
-            }
-          });
-        }
+        //save car in collection
+        await car.save();
+
+        //create new users
+        let user_item = new User({
+          name: faker.name.firstName(),
+          username: faker.internet.userName(),
+          email: faker.internet.email(),
+          gender: faker.name.gender(),
+          age: Math.floor(Math.random() * 90) + 1,
+          address: {
+            street: faker.address.streetName(),
+            city: faker.address.city(),
+            country: faker.address.country(),
+          },
+          car: car,
+        });
+
+        //save users
+        await user_item.save();
       }
-    })
-    .catch((err) => console.log(err));
+    }
+  } catch (error) {
+    res.send(JSON.stringify({ error: error }));
+  }
 
   res.redirect("/");
 };
 
-//Single read
-exports.findUser = (req, res, next) => {
+//----- READ SINGLE USER ------//
+exports.findUser = async (req, res, next) => {
+  //Set header JSON
+  res.setHeader("Content-Type", "application/json");
+
+  //param
   var userName = req.params.userName;
 
-  User.find({ username: userName })
-    .populate({ path: "car", model: "Car" })
-    .exec()
-    .then((user) => {
-      res.setHeader("Content-Type", "application/json");
-      res.send(JSON.stringify({ user }));
-    });
+  //read user
+  try {
+    const user = await User.find({ username: userName })
+      .populate({ path: "car", model: "Car" })
+      .exec();
+
+    //response json user
+    res.send(JSON.stringify({ user }));
+  } catch (error) {
+    res.send(JSON.stringify({ error: error }));
+  }
 };
 
-//Create
-exports.addUser = (req, res, next) => {
+//-------- CREATE USER----------//
+exports.addUser = async (req, res, next) => {
   //Set header JSON
   res.setHeader("Content-Type", "application/json");
   res.json({ requestBody: req.body });
@@ -120,39 +128,34 @@ exports.addUser = (req, res, next) => {
     },
   });
 
-  //save car in collection
-  car
-    .save()
-    .then((res) => console.log("car added", res))
-    .catch((err) => console.log(err));
+  try {
+    //save car in collection
+    await car.save();
 
-  //create new user obj
-  const user = new User({
-    name: name,
-    username: username,
-    email: email,
-    gender: gender,
-    age: age,
-    address: {
-      street: street,
-      city: city,
-      country: country,
-    },
-    car: car,
-  });
+    //create new user obj
+    const user = new User({
+      name: name,
+      username: username,
+      email: email,
+      gender: gender,
+      age: age,
+      address: {
+        street: street,
+        city: city,
+        country: country,
+      },
+      car: car,
+    });
 
-  //save user in collection
-  user
-    .save()
-    .then((res) => {
-      console.log(res);
-    })
-    .catch((err) => console.log(err));
+    //save user in collection
+    await user.save();
+  } catch (error) {
+    res.send(JSON.stringify({ error: error }));
+  }
 };
 
-//------ UPDATE --------//
-
-exports.updateUser = (req, res, next) => {
+//------ UPDATE USER--------//
+exports.updateUser = async (req, res, next) => {
   //Set header to json data input
   res.setHeader("Content-Type", "application/json");
   res.json({ requestBody: req.body });
@@ -173,7 +176,6 @@ exports.updateUser = (req, res, next) => {
   const carYear = req.body.car.year;
   const carPower = req.body.car.specs.power;
   const carFuel = req.body.car.specs.fuel;
-
   //create new car obj
   const car = new Car({
     brand: carBrand,
@@ -185,68 +187,62 @@ exports.updateUser = (req, res, next) => {
     },
   });
 
-  //save car in collection
-  car
-    .save()
-    .then((res) => console.log("car added", res))
-    .catch((err) => console.log(err));
+  try {
+    //save car in collection
+    let carUpdated = await car.save();
 
-  // Find User and his car related
-  User.findOne({ username: usernameParam })
-    .populate({ path: "car", model: "Car" })
-    .exec()
-    .then((user) => {
-      console.log("before", user);
-      console.log("before car ", user.car);
-      return user;
-    })
-    .then((user) => {
-      //Assign new values to user update
-      user.name = name;
-      user.username = username;
-      user.email = email;
-      user.gender = gender;
-      user.age = age;
-      user.car = car;
+    // Find User and his car related
+    const user = await User.findOne({ username: usernameParam })
+      .populate({ path: "car", model: "Car" })
+      .exec();
 
-      let { address } = user;
-      address.street = newStreet;
-      address.city = newCity;
-      address.country = newCountry;
+    //Assign new values to user update
+    user.name = name;
+    user.username = username;
+    user.email = email;
+    user.gender = gender;
+    user.age = age;
+    user.car = car;
 
-      //Save new updated user in collection
-      user
-        .save()
-        .then((res) => console.log(res))
-        .catch((err) => console.log(err));
-    })
-    .catch((err) => console.log(err));
+    let { address } = user;
+    address.street = newStreet;
+    address.city = newCity;
+    address.country = newCountry;
+
+    //Save new updated user in collection
+    let userUpdated = await user.save();
+
+    res.send(JSON.stringify({ userUpdated }));
+  } catch (error) {
+    res.send(JSON.stringify({ error: error }));
+  }
 };
 
-//--------  DELETE ----------//
-exports.deleteUser = (req, res, next) => {
+//--------  DELETE USER ----------//
+exports.deleteUser = async (req, res, next) => {
   //Set header for json response
   res.setHeader("Content-Type", "application/json");
 
   //Param query search
   const username = req.params.userName;
 
-  //Find user
-  User.findOne({ username: username })
-    .populate({ path: "car", model: "Car" })
-    .exec()
-    .then((user) => {
-      //check if user exists
-      if (user) {
-        //user remove
-        user.remove();
+  try {
+    //Find user
+    const user = await User.findOne({ username: username })
+      .populate({ path: "car", model: "Car" })
+      .exec();
 
-        res.send(JSON.stringify({ message: "User deleted successfully" }));
-        res.status(200).end();
-      } else {
-        res.send(JSON.stringify({ message: "Error, user not deleted!!" }));
-        res.status(500).end();
-      }
-    })
-    .catch((err) => console.log(err));
+    if (user) {
+      //user remove
+      await user.remove();
+
+      res.send(JSON.stringify({ message: "User deleted successfully" }));
+      res.status(200).end();
+    } else {
+      res.send(JSON.stringify({ message: "Error, user not deleted!!" }));
+      res.status(500).end();
+    }
+  } catch (error) {
+    res.send(JSON.stringify({ error: error }));
+  }
 };
